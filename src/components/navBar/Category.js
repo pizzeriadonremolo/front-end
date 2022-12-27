0cc29433b3bac932991d5aaaf2ce0f9149b7fd36
RodneyMac <rodneydavidmaciel@gmail.com>
2022-12-26 21:11:00 -0300

    header Logo update

--- src/components/navBar/Category.js
+++ src/components/navBar/Category.js
@@ -1,28 +1,31 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import style from './index.module.css'
-
-
+import style from "./index.module.css";
 
 const Category = ({ category }) => {
-    const location = useLocation();
-    const navigate =useNavigate();
-    return(
-    <>
-    <div onClick={()=>{navigate(`/${category}`)}} className={style.button}
-            style={
-            location.pathname === `/${category}`
-              ? {
-                  backgroundColor: "#5171A5",
-                  color: "white",
-                }
-              : null
-          }
-    >
-        <img src={`/img/${category}.png`} alt={category}/>
-        <p className={style.title}>{category}</p>
-    </div>
-    </>
-    )
-  };
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  export default Category;
+  return (
+    <>
+      <div
+        onClick={() => {
+          navigate(`/${category}`);
+        }}
+        className={style.button}
+        style={
+          location.pathname === `/${category}`
+            ? {
+                backgroundColor: "#5171A5",
+                color: "white",
+              }
+            : null
+        }
+      >
+        <img src={`/img/${category}.png`} alt={category} />
+        <p className={style.title}>{category}</p>
+      </div>
+    </>
+  );
+};
+
+export default Category;
